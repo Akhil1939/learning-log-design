1ac58ed08d517c3bbe661f9aae9f4f1a7cc25ef2
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-07-05 11:30:18 +0530

    module and routing setup complete

---
+++ Learning-Log/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule) },
+   {path:'profile', loadChildren:()=>import('./modules/profile/profile.module').then(m=>m.ProfileModule)},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+  constructor() {
+    console.log('AppRoutingModule loaded.');
+  }
+  
+ }
